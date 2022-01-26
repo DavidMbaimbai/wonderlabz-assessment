14fadaa04351c6897a458960278a6e2171420d08
David Mbaimbai <davymbaimbai@gmail.com>
2022-01-26 09:43:16 +0200

    Project setup

---
+++ src/main/java/com/wonderlabz/assessment/backend/WonderlabzApplication.java
@@ -0,0 +1,13 @@
+package com.wonderlabz.assessment.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderlabzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonderlabzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wonderlabz/assessment/backend/WonderlabzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonderlabz.assessment.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderlabzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
