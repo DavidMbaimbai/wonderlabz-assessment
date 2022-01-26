3f30b20d4edcc69479b7721ae8fcd80433e46fce
David Mbaimbai <davymbaimbai@gmail.com>
2022-01-26 16:09:05 +0200

    Added Eureka annotation in the main application

--- src/main/java/com/wonderlabz/assessment/backend/WonderLabzApplication.java
+++ src/main/java/com/wonderlabz/assessment/backend/WonderLabzApplication.java
@@ -2,8 +2,10 @@ package com.wonderlabz.assessment.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class WonderLabzApplication {
 
 	public static void main(String[] args) {
